ede16828ec947231e66f94a2275b1605aca430fa
mjacker-wsl-ubuntu <mjackerjaime@gmail.com>
2025-04-16 03:07:37 +0530

    updated some solved hackerrank linux shell.

--- 0_Web programming challenges/hackerrank/linux-shell/looping-with-number.sh
+++ 0_Web programming challenges/hackerrank/linux-shell/looping-with-number.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-for i in {1..50};do
+
+# Brace expansion
+for i in {1..5};do
     echo $i
 done
+
+# C-style
+for ((i=1; i<6; i++)); do 
+  echo $i 
+done
+
+# seq (sequence)
+for i in $(seq 1 5); 
+do 
+  echo $i 
+done 
+
