5d40ca7cb5ffb380499bdc47cf0839c91eb19dea
mjacker-wsl-ubuntu <mjackerjaime@gmail.com>
2025-05-03 09:24:07 +0530

    Added sample tests, c language, codewards.

---
+++ 0_Web programming challenges/01 Codewards/c/sample-tests/main.c
@@ -0,0 +1,5 @@
+#include <stdbool.h>
+
+bool is_even(double n) {
+    return (n == (int)n) && ((int)n % 2 == 0);
+}
---
+++ 0_Web programming challenges/01 Codewards/c/sample-tests/test.c
@@ -0,0 +1,18 @@
+#include <criterion/criterion.h>
+#include <stdbool.h> // for bool type return in functions
+
+bool is_even(double n);
+
+void do_test(double n, bool expected) {
+    cr_assert_eq(is_even(n), expected,
+                 "Expected is_even(%.2f) to be %s", n, expected ? "true" : "false");
+}
+
+Test(is_it_even, example_tests) {
+    do_test(0,   true);
+    do_test(0.5, false);
+    do_test(1,   false);
+    do_test(2,   true);
+    do_test(-4,  true);
+}
+
